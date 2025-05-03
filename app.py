33dd8185ccef13773260e38682410d3205821718
greatlifenate <greatlifenate@gmail.com>
2025-05-03 10:56:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,41 @@ def quote():
     )
 
     return jsonify(response.json())
+# Earnings Reports
+@app.route('/earnings')
+def earnings():
+    symbol = request.args.get('symbol')
+    response = requests.get(
+        f'https://finnhub.io/api/v1/stock/earnings?symbol={symbol}&token={FINNHUB_API_KEY}'
+    )
+    return jsonify(response.json())
+
+# ETF Holdings
+@app.route('/etf-holdings')
+def etf_holdings():
+    symbol = request.args.get('symbol')
+    response = requests.get(
+        f'https://finnhub.io/api/v1/etf/holdings?symbol={symbol}&token={FINNHUB_API_KEY}'
+    )
+    return jsonify(response.json())
+
+# Mutual Fund Profile
+@app.route('/mf-profile')
+def mf_profile():
+    symbol = request.args.get('symbol')
+    response = requests.get(
+        f'https://finnhub.io/api/v1/mutual-fund/profile?symbol={symbol}&token={FINNHUB_API_KEY}'
+    )
+    return jsonify(response.json())
+
+# Mutual Fund Holdings
+@app.route('/mf-holdings')
+def mf_holdings():
+    symbol = request.args.get('symbol')
+    response = requests.get(
+        f'https://finnhub.io/api/v1/mutual-fund/holdings?symbol={symbol}&token={FINNHUB_API_KEY}'
+    )
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
